5573c272ab09a586bab69d1715cd94cd4d121de8
Isaac <isaac.hatton@wwt.com>
2020-08-16 00:27:30 -0600

    removed all but white and off led patterns/ animations

--- tmk_core/protocol/arm_atsam/led_matrix_programs.c
+++ tmk_core/protocol/arm_atsam/led_matrix_programs.c
@@ -104,18 +104,25 @@ led_setup_t leds_rainbow_s[] = {
 //The last entry must be { .end = 1 }
 //Add the new animation name to the list below following its format
 
+//White CUSTOM 
+led_setup_t leds_white_custom[] = {
+    { .hs = 0, .he = 100, .rs = 255, .re = 255, .gs = 255, .ge = 255, .bs = 255, .be = 255, .ef = EF_NONE },
+    { .end = 1 },
+};
+
 void *led_setups[] = {
-    leds_rainbow_s,
-    leds_rainbow_ns,
-    leds_teal_salmon,
-    leds_yellow,
-    leds_red,
-    leds_green,
-    leds_blue,
-    leds_white,
-    leds_white_with_red_stripe,
-    leds_black_with_red_stripe,
-    leds_off
+    // leds_rainbow_s,
+    // leds_rainbow_ns,
+    // leds_teal_salmon,
+    // leds_yellow,
+    // leds_red,
+    // leds_green,
+    // leds_blue,
+    // leds_white,
+    // leds_white_with_red_stripe,
+    // leds_black_with_red_stripe,
+    leds_off,
+    leds_white_custom
 };
 
 const uint8_t led_setups_count = sizeof(led_setups) / sizeof(led_setups[0]);
